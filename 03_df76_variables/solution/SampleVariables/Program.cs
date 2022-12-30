dab2c712238e68c125287d6c9c63d4fe1d5f004c
Uaychai Chotjaratwanich <uchotjaratwanich@gmail.com>
2022-12-30 17:47:42 +0700

    2022-12-30

---
+++ 03_df76_variables/solution/SampleVariables/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+    Author: Uaychai Chotjaratwanich
+    Modified Date: 2022-12-30
+*/
+//Declare Variable and Assignment Statement
+int a,b,c;
+a=20;
+int d=20;
+
+//Whole Number
+//Ref: https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/integral-numeric-types
+sbyte x1 = 20;
+byte x2 = 20;
+short x3 = 20;
+ushort x4 = 20;
+int x5 = 20;
+uint x6 = 20;
+long x7 = 20;
+ulong x8 = 20;
+nint x9 = 20;
+nuint x10 = 20;
+
+//Decimal Number
+//Ref: https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/floating-point-numeric-types
+float y1 = 20.5f;
+double y2 = 20.5d;
+decimal y3 = 20.5m;
+
+//Character
+char z1 = 'u';
+
+//Boolean
+bool i1 = true;
+
+//String
+string j1 = "Uaychai";
+
+//Conversion
+long t1 = 20;
+short t2 = 50;
+t1 = t2;
+
+//null
+int? q1 = null;
