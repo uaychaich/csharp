dfdcde97315b2d80b5d097d63742e73fe1468305
Uaychai Chotjaratwanich <13571179+uaychaich@users.noreply.github.com>
2023-01-07 02:19:28 +0000

    2023-01-07

---
+++ 11_25c5_constructor/solution/SampleConstructor/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+    Author: Uaychai Chotjaratwanich
+    Modified Date: 2023-01-07
+*/
+SampleClass sc1 = new SampleClass();Console.WriteLine(sc1.Data1);
+SampleClass sc2 = new SampleClass(700);Console.WriteLine(sc2.Data1);
+
+SampleStructure ss1 = new SampleStructure();Console.WriteLine(ss1.Data1);
+SampleStructure ss2 = new SampleStructure(700);Console.WriteLine(ss2.Data1);
+
+PeopleClass pc1 = new PeopleClass();Console.WriteLine(pc1.FirstName);
+PeopleClass pc2 = new PeopleClass("Uaychai","Chotjaratwanich");Console.WriteLine(pc2.FirstName);
+
+PeopleStructure ps1 = new PeopleStructure();Console.WriteLine(ps1.FirstName);
+PeopleStructure ps2 = new PeopleStructure("Uaychai","Chotjaratwanich");Console.WriteLine(ps2.FirstName);
---
+++ 11_25c5_constructor/solution/SampleConstructor/SampleStructures.cs
@@ -0,0 +1,31 @@
+/*
+    Author: Uaychai Chotjaratwanich
+    Modified Date: 2023-01-07
+*/
+public class SampleStructure
+{
+    //field
+    public int _Data1;
+    //property
+    public int Data1{set=>_Data1=value; get=>_Data1;}
+    //method
+    public void Dosomething(){}
+    //constructor
+    public SampleStructure(){Data1=500;Dosomething();}
+    public SampleStructure(int inputdata){Data1=inputdata;Dosomething();}
+}
+
+public class PeopleStructure
+{
+    //field
+    public string _FirstName, _LastName;
+    //property
+    public string FirstName{set=>_FirstName = value; get=>_FirstName;}
+    public string LastName{set=>_LastName = value;get=>_LastName;}
+    //method
+    public void WriteLog(string log){/*Write log in database*/}
+    //constructor
+    public PeopleStructure(){FirstName="Jon";LastName="Dow";WriteLog("Jon Dow is created");}
+    public PeopleStructure(string inputFirstName, string inputLastName)
+    {FirstName=inputFirstName;LastName=inputLastName;WriteLog($"{FirstName} {LastName} is created");}
+}
