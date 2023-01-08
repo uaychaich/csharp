c6fe4b59cc613646fb3c0f9b0e7fe5b25dec256e
Uaychai Chotjaratwanich <13571179+uaychaich@users.noreply.github.com>
2023-01-08 11:30:21 +0000

    2023-01-08

---
+++ 13_cc5e_access_modifiers/solution/SampleAccessModifier/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+    Author: Uaychai Chotjaratwanich
+    Modified Date: 2023-01-08
+*/
+SampleClass1 sc1 = new SampleClass1();
+Console.WriteLine($"{sc1._Data1} {sc1.Data1} {sc1.getData1()}");
+
+SampleClass1_1 sc11 = new SampleClass1_1();
+Console.WriteLine($"{sc11._Data1} {sc11.Data1} {sc11.getData1()}");
+
+SampleStruct1 ss1 = new SampleStruct1();
+Console.WriteLine($"{ss1._Data1} {ss1.Data1} {ss1.getData1()}");
